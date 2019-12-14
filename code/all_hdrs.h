3e64195fce6b1c613547a5b1fa8834ac12629f5e
Ryan <rnleach@users.noreply.github.com>
2019-12-14 12:35:33 -0700

    Add all_hdrs.h

---
+++ code/all_hdrs.h
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <getopt.h>
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <csv.h>
+#include <curl/curl.h>
